3c787c27fc26fe97bf8b045eb25da054923e9176
Keith Tom <keith.tom@gmail.com>
2013-03-07 22:47:42 -0800

    problem 01: load models

--- problem-01/sinatra/config/environment.rb
+++ problem-01/sinatra/config/environment.rb
@@ -25,9 +25,13 @@ APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
 
-# Set up the controllers and helpers
+# Set up the controllers, models and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
