8ffb9612b0f48a6d8a8996ba06b07218ec0f6073
Keith Tom <keith.tom@gmail.com>
2013-03-08 08:44:49 -0800

    problem-02: remove sinatra skeleton

--- problem-02/sinatra/app/controllers/index.rb
+++
@@ -1,9 +0,0 @@
-get '/' do
-  # Look in app/views/index.erb
-  erb :index
-end
-
-post "/comments" do
-  comment = Comment.create!(params[:comment])
-  body comment.to_json
-end
--- problem-02/sinatra/db/migrate/20130118211810_create_base.rb
+++
@@ -1,8 +0,0 @@
-class CreateBase < ActiveRecord::Migration
-  def change
-    create_table :comments do |table|
-      table.string  :body, :null => false
-      table.timestamps
-    end
-  end
-end
