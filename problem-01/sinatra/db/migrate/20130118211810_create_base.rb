3c46290162f4ed16842d8930ecdc9bcb97aa6e28
Keith Tom <keith.tom@gmail.com>
2013-03-08 00:16:18 -0800

    problem-01: add timestamps to comment

--- problem-01/sinatra/db/migrate/20130118211810_create_base.rb
+++ problem-01/sinatra/db/migrate/20130118211810_create_base.rb
@@ -8,6 +8,7 @@ class CreateBase < ActiveRecord::Migration
     create_table :comments do |table|
       table.integer :user_id, :null => false
       table.string  :body, :null => false
+      table.timestamps
     end
   end
 end
