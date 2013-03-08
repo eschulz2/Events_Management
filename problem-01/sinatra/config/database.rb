7080a54012919e2d873b928dde12f60bbc5e8e61
Keith Tom <keith.tom@gmail.com>
2013-03-07 22:18:02 -0800

    problem-01: setup db and models.  we are more interested in web

--- problem-01/sinatra/config/database.rb
+++ problem-01/sinatra/config/database.rb
@@ -13,31 +13,9 @@ Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   autoload ActiveSupport::Inflector.camelize(filename), model_file
 end
 
-# We have to do this in case we have models that inherit from each other.
-# If model Student inherits from model Person and app/models/student.rb is
-# required first, it will throw an error saying "Person" is undefined.
-#
-# With this lazy-loading technique, Ruby will try to load app/models/person.rb
-# the first time it sees "Person" and will only throw an exception if
-# that file doesn't define the Person class.
-
-# Heroku controls what database we connect to by setting the DATABASE_URL environment variable
-# We need to respect that if we want our Sinatra apps to run on Heroku without modification
-db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
-
-DB_NAME = db.path[1..-1]
-
-# Note:
-#   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
-#   if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
-#   to :development
+DB_NAME = 'db/sqlite3.db'
 
 ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :port     => db.port,
-  :username => db.user,
-  :password => db.password,
+  :adapter  => 'sqlite3',
   :database => DB_NAME,
-  :encoding => 'utf8'
 )
